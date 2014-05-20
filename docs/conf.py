8e176f307deef15f0ba544219088617acdeb8962
Christian Winther <cw@bownty.com>
2014-05-20 21:46:47 +0200

    bump doc config

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ source_suffix = '.rst'
 master_doc = 'contents'
 
 # General information about the project.
-project = u'puppet-php'
+project = u'puppet-php - PHP module for Puppet'
 copyright = u'2014, Christian Winther'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -165,22 +165,22 @@ html_sidebars = {
 #html_additional_pages = {}
 
 # If false, no module index is generated.
-#html_domain_indices = True
+html_domain_indices = True
 
 # If false, no index is generated.
-#html_use_index = True
+html_use_index = True
 
 # If true, the index is split into individual pages for each letter.
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = True
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the

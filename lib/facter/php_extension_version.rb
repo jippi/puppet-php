b3abe4e106847821f507cab0618b90dc7608cb23
Tony Findeisen <github@it-ony.de>
2014-11-21 10:28:20 -0200

    added fact for php_extension_version

---
+++ lib/facter/php_extension_version.rb
@@ -0,0 +1,11 @@
+Facter.add(:php_extension_version) do
+  confine :kernel => :linux
+  setcode do
+    version = Facter::Util::Resolution.exec('/usr/bin/env php -i | grep "PHP Extension =" | cut -d " " -f 4 2>/dev/null')
+    if version
+      version
+    else
+      nil
+    end
+  end
+end

3dd30453da6e63dc9c984cefe80d24b023fd0ee2
Tony Findeisen <github@it-ony.de>
2014-11-26 15:39:52 -0200

    don't show an error if php is not installed

--- lib/facter/php_extension_version.rb
+++ lib/facter/php_extension_version.rb
@@ -1,7 +1,7 @@
 Facter.add(:php_extension_version) do
   confine :kernel => :linux
   setcode do
-    version = Facter::Util::Resolution.exec('/usr/bin/env php -i | grep "PHP Extension =" | cut -d " " -f 4 2> /dev/null')
+    version = Facter::Util::Resolution.exec('/usr/bin/env php -i 2> /dev/null | grep "PHP Extension =" | cut -d " " -f 4')
     if version
       version
     else

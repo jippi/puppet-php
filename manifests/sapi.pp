6c8b7638dad584697831636c5ea055c9231cf888
Goran Miskovic <schkovich@gmail.com>
2015-04-27 02:54:13 +0200

    more conditions

--- manifests/sapi.pp
+++ manifests/sapi.pp
@@ -3,17 +3,19 @@ define php::sapi (
   $ensure,
   $priority,
 ) {
-  if defined(Service[$php::fpm::params::service_name]) {
-    $disenable = $sapi
-  } elsif defined(Package[$php::apache::params::package]) {
-    $disenable = $sapi
+  if defined(Service[$php::fpm::params::service_name]) and ("fpm${extension}" == $title) {
+    $disenable = $title
+  } elsif defined(Package[$php::apache::params::package]) and ("apache2${extension}" == $title) {
+    $disenable = $title
+  } elsif ("cli${extension}" == $title) {
+    $disenable = $title
   } else {
-    $disenable = $sapi
+    $sapi = delete($title, $extension)
+    fail("Unsuported sapi: ${sapi}")
   }
   php::extension::disenable { $disenable:
     extension   => $extension,
     ensure      => $ensure,
     pripriority => $priority,
-    unless => empty($disenable)
   }
 }

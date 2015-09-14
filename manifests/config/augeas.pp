2cebb9c7e53144ef18dc70f1ef6275c940631c19
Igor Galić <i.galic@brainsware.org>
2015-09-14 12:37:09 +0200

    move augeas deployment into autoload path
    this way our module works transparently with all versions of puppet
    without having to change any paths.

--- manifests/config/augeas.pp
+++ manifests/config/augeas.pp
@@ -3,14 +3,11 @@ define php::config::augeas (
   $file,
   $config,
 ) {
-  include php::augeas
   include php::params
 
   augeas { "php-${name}-config":
-    incl      => $file,
-    changes   => $config,
-    load_path => $php::params::augeas_contrib_dir,
-    lens      => 'PHP.lns',
-    require   => Class['php::augeas']
+    incl    => $file,
+    changes => $config,
+    lens    => 'PHP.lns',
   }
 }
